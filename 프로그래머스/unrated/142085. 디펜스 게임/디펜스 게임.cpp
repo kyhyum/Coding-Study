6514d866fec9b3c933dcf0a4f6a4a811d95a5599
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-10 19:11:40 +0900

    [unrated] Title: 디펜스 게임, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
@@ -1,34 +1,46 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
 using namespace std;
 
-int solution(int n, int k, vector<int> enemy) {
-    priority_queue<int,vector<int>,greater<int>> que;
-    cout << enemy.size();
-    if(k >= enemy.size()){
-        return enemy.size();
-    }
-    for(int i = 0 ; i < k ; i++){
-        que.push(enemy[i]);
-    }
-    int i = k;
-    for(i ; i < enemy.size() ; i++){
-        if(que.top() < enemy[i]){
-            n -= que.top();
-            if(n<0){
-                return i;
-            }else{
-                que.pop();
-                que.push(enemy[i]);
+int minimum = 0;
+vector<int> enemys;
+void dfs(int num, int k, int idx, int count){
+    int plusidx = idx + 1;
+    int pluscount = count + 1;
+    int minusk = k - 1;
+    
+    //무적권 쓸 수 있을 때
+    if (k > 0){
+        //사용
+        dfs(num, minusk, plusidx, pluscount);
+        //사용 X
+        num -= enemys[idx];
+        
+        if(num < 0 || idx >= enemys.size() - 1){
+            if(minimum < count){
+                minimum = count;
             }
+            return;
         }else{
-            n -= enemy[i];
-            if(n<0){
-                return i;
+            dfs(num,k,plusidx,pluscount);    
+        }
+    }else{  //무적권 쓸 수 없을 때
+         num -= enemys[idx];
+        
+        if(num < 0 || idx >= enemys.size() - 1){
+            if(minimum < count){
+                minimum = count;
             }
+            return;
+        }else{
+            dfs(num,k,plusidx,pluscount);    
         }
     }
-    return enemy.size();
+    
+}
+int solution(int n, int k, vector<int> enemy) {
+    enemys = enemy;
+    dfs(n,k,0,0);
+    return minimum;
 }
