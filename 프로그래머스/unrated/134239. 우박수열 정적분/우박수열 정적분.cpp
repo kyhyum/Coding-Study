e82ba476f2739dc575838c065a9192041b84f87d
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-10 20:14:20 +0900

    [unrated] Title: 우박수열 정적분, Time: 1.05 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+double width(double up, double down){
+    return (up+down)/2;
+}
+
+vector<double> solution(int k, vector<vector<int>> ranges) {
+    vector<double> nums;
+    vector<double> widths;
+    vector<double> answer;
+    while(k != 1){
+        nums.push_back(k);
+        if(k % 2 == 0){
+            k /= 2;
+        }else{
+            k = k*3 + 1;
+        }
+    }
+    nums.push_back(1);
+    
+    for(int i = 0 ; i < nums.size() - 1 ; i++){
+        widths.push_back(width(nums[i],nums[i+1]));
+    }
+    
+    for(int i = 0 ; i < ranges.size() ; i++){
+        double x = 0;
+        double left = ranges[i][0];
+        double right = ranges[i][1];
+        
+        if(left > widths.size() + right){
+            answer.push_back(-1);
+            continue;
+        }
+        
+        for(left ; left < widths.size() + right ; left++){
+            x += widths[left];
+        }
+        
+        answer.push_back(x);
+    }
+    return answer;
+}
