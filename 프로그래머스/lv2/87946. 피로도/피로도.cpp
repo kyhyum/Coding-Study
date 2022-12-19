b8e46a5ebe72d529bb13967da5e7953665b23ed8
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-19 16:16:08 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer = -1;    
+void dfs(vector<bool> visited, int k, vector<vector<int>> dungeons, int count){
+    for(int i = 0 ; i < dungeons.size() ; i++){
+        if(dungeons[i][0] <= k && k - dungeons[i][1] >= 0 && visited[i] == false){
+            visited[i] = true;
+            count++;
+            if(answer < count){
+                answer = count;
+            }
+            dfs(visited, k - dungeons[i][1], dungeons, count);
+            visited[i] = false;
+            count--;
+        }
+    }
+}
+int solution(int k, vector<vector<int>> dungeons) {
+    vector<bool> visited;
+    for(int i = 0 ; i < dungeons.size() ; i++){
+        visited.push_back(false);
+    }
+    dfs(visited, k, dungeons, 0);
+    return answer;
+}
