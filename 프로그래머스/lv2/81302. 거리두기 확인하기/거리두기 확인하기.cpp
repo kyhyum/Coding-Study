bb539be058a3a668154c866d72116e76f4e2bbea
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-22 22:53:12 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -1,49 +1,57 @@
-#include <vector>
 #include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
 using namespace std;
-int check(vector<string>& place) {
-    vector<pair<int, int>> persons;
-    for (int x = 0, y; x < place.size(); ++x)
-        for (y = 0; y < place[x].size(); ++y)
-            if (place[x].at(y) == 'P')               persons.push_back(make_pair(x, y));
-    if (persons.size() <= 1)
-        return 1;
-    for (int x = 0, y; x < persons.size() - 1; ++x) {
-        for (y = x + 1; y < persons.size(); ++y) {
-            auto& t1 = persons[x];
-            auto& t2 = persons[y];
-            const auto& distance = abs(t1.first - t2.first) + abs(t1.second - t2.second);
-            if (distance == 1)
-                return 0;
-            if (distance == 2) {
-                if (t1.first == t2.first) {           // 좌/우 로 앉아 있는 경우
-                    if (place[t1.first].at(min(t1.second, t2.second) + 1) != 'X')
-                        return 0;
+
+vector<vector<string>> place;
+
+int check(int i, int array[5][5]) {
+        for (int j = 0; j < 5; j++) {
+            for (int w = 0; w < 5; w++) {
+                cout << array[j][w] << " ";
+                if (array[j][w] <= -2) {
+                    return 0;
                 }
-                else if (t1.second == t2.second) {    // 상/하 로 앉아 있는 경우
-                    if (place[min(t1.first, t2.first) + 1].at(t1.second) != 'X')
-                        return 0;
+            }
+            cout << endl;
+        }
+    return 1;
+}
+vector<int> solution(vector<vector<string>> places) {
+    place = places;
+    vector<int> answer;
+    for (int i = 0; i < 5; i++) {
+        int array[5][5] = { 0, };
+        for (int j = 0; j < 5; j++) {
+            for (int w = 0; w < 5; w++) {
+                if (places[i][j][w] == 'X') {
+                    array[j][w] += 4;
                 }
-                else {                                // 대각으로 앉아 있는 경우
-                    if ((t1.first < t2.first && t1.second < t2.second) || (t2.first < t1.first && t2.second < t1.second)) {
-                        if (place[min(t1.first, t2.first)].at(max(t1.second, t2.second)) != 'X' ||
-                            place[max(t1.first, t2.first)].at(min(t1.second, t2.second)) != 'X')
-                            return 0;
-                    }
-                    else {
-                        if (place[min(t1.first, t2.first)].at(min(t1.second, t2.second)) != 'X' ||
-                            place[max(t1.first, t2.first)].at(max(t1.second, t2.second)) != 'X')
-                            return 0;
+                else if (places[i][j][w] == 'P') {
+                    int dx[4] = { 0,1,0,-1 };
+                    int dy[4] = { 1,0,-1,0 };
+                    for (int k = 0; k < 4; k++) {
+                        int nx = dx[k] + j;
+                        int ny = dy[k] + w;
+                        if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+                            if (places[i][nx][ny] == 'P') {
+                                answer.push_back(0);
+                                w = 5;
+                                j = 5;
+                                break;
+                            }
+                            array[nx][ny] += -1;
+                        }
                     }
                 }
             }
         }
+        if (i != answer.size() - 1) {
+            answer.push_back(check(i, array));
+        }
+        cout << endl;
     }
-    return 1;
-}
-vector<int> solution(vector<vector<string>> places) {
-    vector<int> ans;
-    for (int t = 0, x, y; t < places.size(); ++t)
-        ans.push_back(check(places[t]));
-    return ans;
+        return answer;
 }
