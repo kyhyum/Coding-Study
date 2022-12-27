4e831276fac630ce901dc8ae39bb78cd423b50cc
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-27 10:31:30 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.75 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,28 +1,11 @@
-#include <string>
 #include <vector>
-#include <iostream>
-#include <cmath>
 using namespace std;
-long long GetBitCounts(long long a){
-    long long count = 0; 
-    while(1){
-        if(a % 2 == 0) return count;
-        count++;
-        a /= 2;
-    }
-}
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     for(int i = 0 ; i < numbers.size() ; i++){
-        long long num = numbers[i];
-        if(num % 2 == 0){
-            answer.push_back(++num);
-        }else{
-            int counts = GetBitCounts(num);
-            cout << counts;
-            answer.push_back(num + pow(2,counts) - pow(2,counts-1));
-        }
-        
+        long long bit = 1;
+        while ((numbers[i] & bit) > 0) bit <<= 1;
+        answer.push_back(numbers[i] + bit - (bit >> 1));
     }
     return answer;
 }
