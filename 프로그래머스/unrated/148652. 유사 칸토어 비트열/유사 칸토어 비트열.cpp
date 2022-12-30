aa28b3523cb6194fc905d08154b22d2d8cf8cce6
kyhyum <86953615+kyhyum@users.noreply.github.com>
2022-12-30 23:59:24 +0900

    Update 유사 칸토어 비트열.cpp

--- 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
@@ -1,28 +1,24 @@
 #include <string>
-#include <vector>
-#include <cmath>
 
 using namespace std;
 
-int f(int n, long long x) {
+int whatbit(int n, long long x) {
     if (n == 1) {
         return x % 5 != 2;
     }
     else {
-        if (!f(n - 1, x / 5)) {
+        if (!whatbit(n - 1, x / 5)) {
             return 0;
         }
         else {
             return x % 5 != 2;
         }
     }
-
-    return n == 1 ? x % 5 != 3 : !f(n - 1, x / 5) ? 0 : x % 5 != 3;
 }
 int solution(int n, long long l, long long r) {
     int answer = 0;
     for (long long i = l - 1; i < r; i++) {
-        answer += f(n, i);
+        answer += whatbit(n, i);
     } 
     return answer;
 }
