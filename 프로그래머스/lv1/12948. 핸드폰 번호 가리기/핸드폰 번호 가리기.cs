b0ff117e203e244956d3e45639823f759ae6bd1b
kyhyum <86953615+kyhyum@users.noreply.github.com>
2023-09-22 18:48:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.26 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string solution(string phone_number) {
+        string answer = null;
+        for(int i= 0; i<phone_number.Length - 4 ;i++){
+            answer += "*";
+        }
+        for(int i=phone_number.Length - 4 ; i<phone_number.Length ;i++){
+            answer += phone_number[i];
+        }
+        return answer;
+    }
+}
